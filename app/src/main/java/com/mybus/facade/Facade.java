fe066fc0216f2569aa235d8c2e1647b670a40e36
lucasdelio <lucasdelio@gmail.com>
2016-05-11 11:29:03 -0300

    MYB-122: Created Facade class

---
+++ app/src/main/java/com/mybus/facade/Facade.java
@@ -0,0 +1,36 @@
+package com.mybus.facade;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Facade {
+
+    /**
+     *
+     * @param constraint
+     * @return
+     */
+    public JSONArray findStreets(String constraint) {
+        return null;
+    }
+
+    /**
+     *
+     * @param origin
+     * @param destiny
+     * @return
+     */
+    public JSONArray searchRoutes(LatLng origin, LatLng destiny) {
+        return null;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public JSONObject searchRoads() {
+        return null;
+    }
+}
