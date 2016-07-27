758589cb8c52b5772d97eb8732cf1c543a56cf43
Julian Gonzalez <jgonzalez@devspark.com>
2016-07-27 13:51:53 -0300

    Fixed unit test

--- app/src/test/java/com/mybus/model/CompleteRouteUnitTest.java
+++ app/src/test/java/com/mybus/model/CompleteRouteUnitTest.java
@@ -21,11 +21,10 @@ public class CompleteRouteUnitTest {
         JSONObject jsonMockedResponse = FileLoaderHelper.loadJSONObjectFromResource(this, "complete_routes/complete_route_id_line_2.json");
         CompleteBusRoute parsedCompleteRoute = CompleteBusRoute.parseOneWayBusRoute(jsonMockedResponse);
         // Checks fields
-        assertEquals(parsedCompleteRoute.getType(), 1);
         assertEquals(parsedCompleteRoute.getColor(), "#00338e");
 
         // Checks PointList
-        assertNotNull(parsedCompleteRoute.getPointList());
-        assertEquals(parsedCompleteRoute.getPointList().size(), 126);
+        assertNotNull(parsedCompleteRoute.getGoingPointList());
+        assertEquals(parsedCompleteRoute.getGoingPointList().size(), 126);
     }
 }
