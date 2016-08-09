7d8e7907035e5127c34891635b55c854d43a9db9
lucasdelio <lucasdelio@gmail.com>
2016-08-09 13:05:13 -0300

    MYB-238: Updated Bus Line layout

--- app/src/main/java/com/mybus/view/BusLineItemViewHolder.java
+++ app/src/main/java/com/mybus/view/BusLineItemViewHolder.java
@@ -3,6 +3,7 @@ package com.mybus.view;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.mybus.R;
@@ -15,6 +16,7 @@ public class BusLineItemViewHolder extends RecyclerView.ViewHolder {
 
     public TextView mBusLineName;
     public ImageView mBusLineIcon;
+    public LinearLayout mBusLineColorRectangle;
     private BusLineListItemListener mItemClickListener;
 
     public BusLineItemViewHolder(View itemView, BusLineListItemListener listener) {
@@ -22,6 +24,7 @@ public class BusLineItemViewHolder extends RecyclerView.ViewHolder {
         mItemClickListener = listener;
         mBusLineName = (TextView) itemView.findViewById(R.id.bus_line_name);
         mBusLineIcon = (ImageView) itemView.findViewById(R.id.bus_line_icon);
+        mBusLineColorRectangle = (LinearLayout) itemView.findViewById(R.id.bus_line_color_rectangle);
         itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
