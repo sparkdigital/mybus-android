21840802101427837bb832ad5f0f4eecf8100d13
lucasdelio <lucasdelio@gmail.com>
2016-10-28 10:48:19 -0300

    MYB-330: Update before pull req

--- app/src/main/java/com/mybus/view/CompoundSearchBox.java
+++ app/src/main/java/com/mybus/view/CompoundSearchBox.java
@@ -30,8 +30,9 @@ public class CompoundSearchBox extends FrameLayout {
         @Override
         public void onClick(View v) {
             if (mListener != null) {
-                if (!getResString(R.string.from_hint).equals(mFromTextView.getText().toString())) {
-                    mListener.onFromClick(mFromTextView.getText().toString());
+                String fromText = mFromTextView.getText().toString();
+                if (!getResString(R.string.from_hint).equals(fromText)) {
+                    mListener.onFromClick(fromText);
                 }
             }
         }
@@ -40,8 +41,9 @@ public class CompoundSearchBox extends FrameLayout {
         @Override
         public void onClick(View v) {
             if (mListener != null) {
-                if (!getResString(R.string.to_hint).equals(mToTextView.getText().toString())) {
-                    mListener.onToClick(mToTextView.getText().toString());
+                String toText = mToTextView.getText().toString();
+                if (!getResString(R.string.to_hint).equals(toText)) {
+                    mListener.onToClick(toText);
                 }
             }
         }
