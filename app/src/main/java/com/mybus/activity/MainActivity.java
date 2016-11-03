ad6f9cf0b6cdd85c1ac6521581496aef3f70ae46
lucasdelio <lucasdelio@gmail.com>
2016-11-03 18:00:25 -0300

    MYB-344: Fix destination autocomplit in MainActivity

--- app/src/main/java/com/mybus/activity/MainActivity.java
+++ app/src/main/java/com/mybus/activity/MainActivity.java
@@ -689,6 +689,13 @@ public class MainActivity extends BaseMyBusActivity implements OnMapReadyCallbac
     }
 
     @Override
+    protected void onPause() {
+        mCompoundSearchBox.setToAddress("");
+        mCompoundSearchBox.setFromAddress("");
+        super.onPause();
+    }
+
+    @Override
     public int getLayoutToInflate() {
         return R.layout.activity_main;
     }
